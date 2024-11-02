d24c4ffa2679011962a3ed9b2bf73e3754f74c52
ToniTrum <kulahszjan@gmail.com>
2024-11-03 00:58:37 +1000

    add orbital animation

--- src/jsx/Main/Satellites.jsx
+++ src/jsx/Main/Satellites.jsx
@@ -1,8 +1,100 @@
-import { img } from 'framer-motion/client'
-import reactLogo from '../../img/react-logo.svg'
+import '../../css/Main/Satellites.css'
+import { motion } from 'framer-motion'
+import { useState, useEffect } from 'react'
 
-const Satellites = () => {
+import reactLogo from '../../img/react-logo.svg'
+import viteLogo from '../../img/vite-logo.svg'
+
+const Satellites = ({ellipseRef}) => {
+    const [ellipseData, setEllipseData] = useState(null)
+    const [satelliteRadius, setSatelliteRadius] = useState(0)
+    const [angle, setAngle] = useState(0)
+    const [satellitePosition, setSatellitePosition] = useState({ x: 0, y: 0 })
+
+    /* useEffect -- это хук, который можно использовать для замены некоторых 
+       методов жизненного цикла классового компонента. 
+       Первый аргумент — это функция обратного вызова, для которой будут 
+       выполняться побочные эффекты 
+       Второй аргумент – массив зависимостей */ 
+    useEffect(() => {
+        const updateEllipseData = () => {
+            if (ellipseRef.current) {
+                const rx = ellipseRef.current.rx.baseVal.value;
+                const ry = ellipseRef.current.ry.baseVal.value;
+                const cx = ellipseRef.current.cx.baseVal.value;
+                const cy = ellipseRef.current.cy.baseVal.value;
+
+                setEllipseData({
+                    "rx": rx,
+                    "ry": ry,
+                    "cx": cx,
+                    "cy": cy
+                })
+
+                setSatelliteRadius(
+                    (ry + ry * 4) / 50 // 4% от родительского элемента
+                )
+            }
+        }
+
+        updateEllipseData()
+        /* Обработчик события изменения размера окна */ 
+        window.addEventListener('resize', updateEllipseData)
+
+        return () => {
+            window.removeEventListener('resize', updateEllipseData);
+        }
+    }, [ellipseRef])
+
+
+    useEffect(() => {
+        if (!ellipseData) return
+
+        const computePosition = () => {
+            const angleInRadians = angle * Math.PI / 180
+            // Эксцентриситент
+            const eccentricity = Math.sqrt(1 - Math.pow(ellipseData.ry / ellipseData.rx, 2))
+            // Радиус эллипса в выбранной точке
+            const ellipseRadiusAtSelectedPoint = ellipseData.ry / Math.sqrt(1 - Math.pow(eccentricity * Math.cos(angleInRadians), 2))
+    
+            const posX = ellipseData.cx + ellipseRadiusAtSelectedPoint * Math.cos(angleInRadians) - satelliteRadius
+            const posY = ellipseData.cy + ellipseRadiusAtSelectedPoint * Math.sin(angleInRadians) - satelliteRadius
+    
+            setSatellitePosition({
+                "x": posX,
+                "y": posY
+            })
+        }
+
+        computePosition()
+    }, [angle, ellipseData, satelliteRadius])
+
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setAngle((prevAngle) => (prevAngle + 1) % 360)
+        }, 100) // примерно 60 FPS
+
+        return () => clearInterval(interval)
+    }, [])
+
     return (
-        <img className='section-orbit__satellite' src={reactLogo} alt="React Logo" />
+        <>
+        <motion.img
+        className='section-orbit__satellite' 
+        src={reactLogo} 
+        alt="React Logo"
+        animate={{
+            y: satellitePosition.y,
+            x: satellitePosition.x
+        }}
+        transition={{
+            type: "spring", 
+            stiffness: 50 
+        }} />
+        {/* <motion.img className='section-orbit__satellite' src={viteLogo} alt="React Logo" /> */}
+        </>
     )
 }
+
+export default Satellites
--- src/jsx/Main/SectionOrbit.jsx
+++ src/jsx/Main/SectionOrbit.jsx
@@ -1,17 +1,24 @@
+import { useRef } from 'react';
 import '../../css/Main/SectionOrbit.css'
+import Satellites from './Satellites.jsx';
 
 const SectionOrbit = () => {
+	/* useRef -- позволяет сохранить некоторый объект, который можно 
+	   изменять и который хранится в течение всей жизни компонента. */
+	const ellipseRef = useRef("ellipse")
+
 	return (
 		<section className='section-orbit'>
 			<svg>
 				<ellipse
+					ref={ellipseRef}
 					cx="50%"  // Координата X центра эллипса
 					cy="50%"  // Координата Y центра эллипса
-					rx="40%"  // Радиус по оси X
-					ry="45%"  // Радиус по оси Y
+					rx="40%"  // Большая полуось
+					ry="45%"  // Малая полуось
 				/>
 			</svg>
-
+			<Satellites ellipseRef={ellipseRef} />
 		</section>
 	);
 };
